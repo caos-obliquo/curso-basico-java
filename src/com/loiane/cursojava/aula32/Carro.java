a755ad2d07684cd97133a301a0fa71f8de760069
beto bettini <caos_obliquo@proton.me>
2025-04-30 19:13:57 -0300

    metodos get e set (encapsulamento)

---
+++ src/com/loiane/cursojava/aula32/Carro.java
@@ -0,0 +1,50 @@
+package com.loiane.cursojava.aula32;
+
+public class Carro {
+
+    private String marca;
+    private String modelo;
+    private int numeroDePassageiros;
+    private double capacidadeCombustivel;
+    private double consumoCombustivel;
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getNumeroDePassageiros() {
+        return numeroDePassageiros;
+    }
+
+    public void setNumeroDePassageiros(int numeroDePassageiros) {
+        this.numeroDePassageiros = numeroDePassageiros;
+    }
+
+    public double getCapacidadeCombustivel() {
+        return capacidadeCombustivel;
+    }
+
+    public void setCapacidadeCombustivel(double capacidadeCombustivel) {
+        this.capacidadeCombustivel = capacidadeCombustivel;
+    }
+
+    public double getConsumoCombustivel() {
+        return consumoCombustivel;
+    }
+
+    public void setConsumoCombustivel(double consumoCombustivel) {
+        this.consumoCombustivel = consumoCombustivel;
+    }
+}
