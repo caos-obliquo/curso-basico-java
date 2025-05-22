d18898d26cc3c409cf686a8d7545f1b90bf43fb8
beto bettini <caos_obliquo@proton.me>
2025-05-22 10:54:18 -0300

    exercicio 03 - static

--- src/com/loiane/cursojava/aula34/labs/Calculadora.java
+++ src/com/loiane/cursojava/aula34/labs/Calculadora.java
@@ -27,4 +27,20 @@ public class Calculadora {
 
         return total;
     }
+
+    // 5! = 5 * 4 * 3 * 2 * 1 = 120
+    // 0! = 1
+    public static int fatorial(int num) {
+
+        if (num == 0) {
+            return 1;
+        }
+
+        int total = 1;
+        for (int i = num; i > 1; i--) {
+            total *= i;
+        }
+
+     return total;
+    }
 }
