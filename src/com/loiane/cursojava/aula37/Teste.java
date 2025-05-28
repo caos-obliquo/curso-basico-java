81e3d8b36934b6c24cd2610d3000c5ae4f2f1a59
beto bettini <caos_obliquo@proton.me>
2025-05-28 00:16:00 -0300

    heranca

---
+++ src/com/loiane/cursojava/aula37/Teste.java
@@ -0,0 +1,26 @@
+package com.loiane.cursojava.aula37;
+
+public class Teste {
+    public static void main(String[] args) {
+        // Herança
+        // É um/É uma
+
+        /*Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        // pessoa.get
+
+        aluno.setNome("Mary");
+
+        // professor.setTelefoneCelular(telefoneCelular);*/
+
+        Pessoa aluno = new Aluno();
+        // aluno.get
+
+        Pessoa professor = new Professor();
+        // professor.get
+    }
+}
